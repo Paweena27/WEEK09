c8ae42c322dd5122df2ec509f8f5b05ac887ec35
Paweena27 <59030185@kmitl.ac.th>
2017-10-21 16:24:29 +0700

    W09P03

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -29,6 +29,9 @@ namespace GDIPlus_1
             Pen somepen = new Pen(Color.FromArgb( 255,120,200));
             g.DrawEllipse(somepen, 20, 20, 200, 200);
             somepen.Dispose();
+
+            Color col = Color.FromName("LigthGreen");
+            this.BackColor = col;
         }
     }
 }
